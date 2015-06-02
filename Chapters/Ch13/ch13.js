6cffeee947920f74b5ee9613048cb8e8729fd7f9
burtality69 <jamesrmccallum@gmail.com>
2015-06-02 16:01:51 -0700

    Ch14 ex2

--- Chapters/Ch13/ch13.js
+++ Chapters/Ch13/ch13.js
@@ -1,3 +1,4 @@
+;
 var MOUNTAINS = [
     { name: "Kilimanjaro", height: 5895, country: "Tanzania" },
     { name: "Everest", height: 8848, country: "Nepal" },
@@ -7,20 +8,16 @@ var MOUNTAINS = [
     { name: "Denali", height: 6168, country: "United States" },
     { name: "Popocatepetl", height: 5465, country: "Mexico" }
 ];
-
 function buildTable(data) {
     var tbl = document.createElement("table");
     var headers = Object.keys(data[0]);
     var th = document.createElement("tr");
-
     headers.map(function (colname) {
         var p = document.createElement("th");
         p.innerHTML = colname;
         th.appendChild(p);
     });
-
     tbl.appendChild(th);
-
     data.map(function (mtn) {
         var r = document.createElement("tr");
         headers.map(function (key) {
@@ -36,5 +33,53 @@ function buildTable(data) {
     });
     return tbl;
 }
-
 document.body.appendChild(buildTable(MOUNTAINS));
+function byTagName(node, tagName) {
+    var r = node.getElementsByTagName(tagName);
+    var ret = [];
+    for (var i = 0; i < r.length; i++) {
+        ret.push(r[i]);
+    }
+    return ret;
+}
+function moveCat() {
+    var cat = document.getElementById("CAT");
+    var hat = document.getElementById("HAT");
+    var angle = 0;
+    var lastTime = null;
+    var catTop, catLeft, hatTop, hatLeft;
+    function animate(time) {
+        if (lastTime != null) {
+            angle += (time - lastTime) * 0.001;
+        }
+        ;
+        lastTime = time;
+        //Cat 
+        catTop = (Math.sin(angle) * 20);
+        catLeft = (Math.sin(angle) * 200);
+        cat.style.top = catTop + "px";
+        cat.style.left = catLeft + "px";
+        //Hat 
+        hatTop = (Math.asin(angle) * 5);
+        hatLeft = (Math.asin(angle) * 50);
+        hat.style.top = hatTop + "px";
+        hat.style.left = hatLeft + "px";
+        requestAnimationFrame(animate);
+    }
+    requestAnimationFrame(animate);
+}
+document.addEventListener("DOMContentLoaded", function (event) {
+    console.log(byTagName(document.body, "h1").length);
+    // → 1
+    console.log(byTagName(document.body, "span").length);
+    // → 3
+    var para = document.querySelector("p");
+    console.log(byTagName(para, "span").length);
+    var field = document.querySelector("input");
+    field.addEventListener("keydown", function (event) {
+        if (event.keyCode == 81 || event.keyCode == 87 || event.keyCode == 88) {
+            event.preventDefault();
+        }
+    });
+    //moveCat();
+});
--- Chapters/Ch13/ch13.ts
+++ Chapters/Ch13/ch13.ts
@@ -1,8 +1,9 @@
- interface Mountain {
-	 name: string;
-	 height: number;
-	 country: string;
- }
+interface Mountain {
+	name: string;
+	height: number;
+	country: string;
+};
+
  var MOUNTAINS: Array<Mountain> = [
   {name: "Kilimanjaro", height: 5895, country: "Tanzania"},
   {name: "Everest", height: 8848, country: "Nepal"},
@@ -19,7 +20,7 @@ function buildTable(data: Array<Mountain>): HTMLTableElement {
 	var headers: Array<any> = Object.keys(data[0]);
 	var th: HTMLTableRowElement = document.createElement("tr");
 
-	headers.map(function(colname: string) {	
+	headers.map(function(colname: string) {
 		var p: HTMLTableHeaderCellElement = document.createElement("th");
 		p.innerHTML = colname;
 		th.appendChild(p);
@@ -33,11 +34,84 @@ function buildTable(data: Array<Mountain>): HTMLTableElement {
 			var p: HTMLTableDataCellElement = document.createElement("td");
 			p.innerHTML = mtn[key];
 			r.appendChild(p);
-			if(typeof mtn[key] == "number") {p.style.textAlign = "right"}; 
+			if (typeof mtn[key] == "number") { p.style.textAlign = "right" };
 		})
 		tbl.appendChild(r);
-	})	   
-	return tbl;  
-  }
+	})
+	return tbl;
+}
   
-  document.body.appendChild(buildTable(MOUNTAINS));
+document.body.appendChild(buildTable(MOUNTAINS));
+
+
+function byTagName(node: Element, tagName: string): Array<Node> {
+    var r: NodeList = node.getElementsByTagName(tagName);
+	var ret: Array<Node> = [];
+	for (var i =0; i < r.length; i++ ) {
+		ret.push(r[i]);
+	} 
+	return ret;
+}
+
+function moveCat() {
+  var cat: HTMLElement = document.getElementById("CAT");
+  var hat: HTMLElement = document.getElementById("HAT");
+  var angle: number = 0;
+  var lastTime: number = null;
+  var catTop: number, catLeft: number, hatTop: number, hatLeft: number;
+  
+  function animate(time: number) {
+    
+    if (lastTime != null)  { 
+      angle += (time - lastTime) * 0.001; 
+    };
+    
+    lastTime = time;
+    
+    //Cat 
+    catTop = (Math.sin(angle) * 20);
+    catLeft = (Math.sin(angle) * 200);
+    cat.style.top = catTop + "px";
+    cat.style.left = catLeft + "px";
+    
+    //Hat 
+    hatTop = (Math.asin(angle) * 5);
+    hatLeft = (Math.asin(angle) * 50);
+    hat.style.top = hatTop + "px";
+    hat.style.left = hatLeft + "px";
+        
+    requestAnimationFrame(animate);
+  }
+  requestAnimationFrame(animate);
+}
+
+function mousetrails(): void {
+  var trails: Array<SVGCircleElement> 
+  
+  for(var i = 0; i<10; i++) {
+    var s = new SVGCircleElement;
+    s.
+  }
+}
+
+document.addEventListener("DOMContentLoaded", function(event: Event): void { 
+  
+  console.log(byTagName(document.body, "h1").length);
+  // → 1
+  console.log(byTagName(document.body, "span").length);
+  // → 3
+  var para = document.querySelector("p");
+  console.log(byTagName(para, "span").length);
+  
+  var field: Element = document.querySelector("input");
+  field.addEventListener("keydown", function(event: KeyboardEvent) {
+    if (event.keyCode == 81 || event.keyCode == 87 || event.keyCode == 88) {
+      event.preventDefault();
+    }
+  });
+  
+  //moveCat();
+  
+});
+
+
